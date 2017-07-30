21b8ecf21fda758fe613d99fa8e79cf8bda61785
Kalpana Madhalam <mkalpanais@gmail.com>
2017-07-30 19:31:47 +1000

    Added ListContacts component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    id: 'john',
+    name: 'John Doe',
+    email: 'john@mail.com',
+    avatarURL: 'http://localhost:5001/john.jpg'
+  },
+  {
+    id: 'jane',
+    name: 'Jane Doe',
+    email: 'jane@mail.com',
+    avatarURL: 'http://localhost:5001/jane.jpg'
+  },
+  {
+    id: 'joe',
+    name: 'Joe Smith',
+    email: 'joe@mail.com',
+    avatarURL: 'http://localhost:5001/joe.jpg'
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <ListContacts contacts={contacts}/>
+      </div>
     );
   }
 }
