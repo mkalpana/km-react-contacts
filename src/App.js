e38b77b8c9fee87e0d82c94645a4f01932f1b37f
Kalpana Madhalam <mkalpanais@gmail.com>
2017-07-31 20:47:11 +1000

    Moved state inside the component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    id: 'john',
-    name: 'John Doe',
-    email: 'john@mail.com',
-    avatarURL: 'http://localhost:5001/john.jpg'
-  },
-  {
-    id: 'jane',
-    name: 'Jane Doe',
-    email: 'jane@mail.com',
-    avatarURL: 'http://localhost:5001/jane.jpg'
-  },
-  {
-    id: 'joe',
-    name: 'Joe Smith',
-    email: 'joe@mail.com',
-    avatarURL: 'http://localhost:5001/joe.jpg'
-  }
-];
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'john',
+        name: 'John Doe',
+        email: 'john@mail.com',
+        avatarURL: 'http://localhost:5001/john.jpg'
+      },
+      {
+        id: 'jane',
+        name: 'Jane Doe',
+        email: 'jane@mail.com',
+        avatarURL: 'http://localhost:5001/jane.jpg'
+      },
+      {
+        id: 'joe',
+        name: 'Joe Smith',
+        email: 'joe@mail.com',
+        avatarURL: 'http://localhost:5001/joe.jpg'
+      }
+    ]
+  }
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     );
   }
