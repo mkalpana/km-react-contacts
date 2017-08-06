19150e326673313b141775ccc99a6ac7c8d073d5
Kalpana Madhalam <mkalpanais@gmail.com>
2017-08-06 11:16:22 +1000

    Added search text boax as a controlled component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -11,29 +11,48 @@ class ListContacts extends Component {
     contacts: []
   };
 
+  state = {
+    query: ''
+  };
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() });
+  };
+
   render() {
     const { contacts, onDeleteContact } = this.props;
     return (
-      <ol className="contact-list">
-        {
-          contacts.map(contact => (
-            <li key={contact.id} className="contact-list-item">
-              <div className="contact-avatar" style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }} />
-              <div className="contact-details">
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button
-                className="contact-remove"
-                onClick={() => onDeleteContact(contact)}
-              >Remove
-              </button>
-            </li>
-          ))
-        }
-      </ol>
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search contacts"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className="contact-list">
+          {
+            contacts.map(contact => (
+              <li key={contact.id} className="contact-list-item">
+                <div className="contact-avatar" style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }} />
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>{contact.email}</p>
+                </div>
+                <button
+                  className="contact-remove"
+                  onClick={() => onDeleteContact(contact)}
+                >Remove
+                </button>
+              </li>
+            ))
+          }
+        </ol>
+      </div>
     );
   }
 }
