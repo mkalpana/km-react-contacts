ab8ae3228c657a15d372f0af89b846910a1f0766
Kalpana Madhalam <mkalpanais@gmail.com>
2017-08-06 12:11:24 +1000

    Added search functionality.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 class ListContacts extends Component {
   static propTypes = {
@@ -21,6 +23,17 @@ class ListContacts extends Component {
 
   render() {
     const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
+    let showingContacts = [];
+
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i');
+      showingContacts = contacts.filter(contact => match.test(contact.name));
+    } else {
+      showingContacts = contacts;
+    }
+    showingContacts.sort(sortBy('name'));
+
     return (
       <div className="list-contacts">
         <div className="list-contacts-top">
@@ -28,13 +41,13 @@ class ListContacts extends Component {
             className="search-contacts"
             type="text"
             placeholder="Search contacts"
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
         <ol className="contact-list">
           {
-            contacts.map(contact => (
+            showingContacts.map(contact => (
               <li key={contact.id} className="contact-list-item">
                 <div className="contact-avatar" style={{
                   backgroundImage: `url(${contact.avatarURL})`
