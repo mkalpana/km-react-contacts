222f51eb527e9854e5b8201dd41314e732f1764c
Kalpana Madhalam <mkalpanais@gmail.com>
2017-08-06 13:55:00 +1000

    Fetched data from server.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'john',
-        name: 'John Doe',
-        email: 'john@mail.com',
-        avatarURL: 'http://localhost:5001/john.jpg'
-      },
-      {
-        id: 'jane',
-        name: 'Jane Doe',
-        email: 'jane@mail.com',
-        avatarURL: 'http://localhost:5001/jane.jpg'
-      },
-      {
-        id: 'joe',
-        name: 'Joe Smith',
-        email: 'joe@mail.com',
-        avatarURL: 'http://localhost:5001/joe.jpg'
-      }
-    ]
+    contacts: []
   };
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts });
+    })
+  }
 
   removeContact = (contact) => {
     this.setState((state) => ({
